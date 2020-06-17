a5b9c0b3a233fd91f280f1cb5cc3087cf0d6dcde
Rasinkani <37532501+Rasinkani@users.noreply.github.com>
2020-06-18 01:10:12 +0530

    Update Assignment.cpp

--- Assignment/Assignment/Assignment.cpp
+++ Assignment/Assignment/Assignment.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char** argv) {
 	time_t t;
 	int rnd_num[512];
-	int i,j, num_loop;
+	int new_mul_arr[6];
+	int i,j,loop_begin,loop_end;
 	int *arr[5];
 	MPI_Init(&argc, &argv);
 	MPI_Status status;
@@ -13,10 +14,11 @@ int main(int argc, char** argv) {
 	int size;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	//my_rank is rak of the process
+	//my_rank is rank of the process
 	int my_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	num_loop = my_rank * 6;
+	loop_begin = (my_rank - 1) * 6;
+	loop_end = my_rank * 6;
 	int rnd_numbers = 6 * (size - 1);
 	if (my_rank == 0) {
 		printf("num of rnd numbers %d \n", rnd_numbers);
@@ -26,48 +28,36 @@ int main(int argc, char** argv) {
 			rnd_num[i] = rand();
 			printf(" %d ,  ", rnd_num[i]);
 		}
-		
-		//for (i = 1; i < size; i++) {
-			//arr[i] = &rnd_num[i * 6];
+		printf("\n\n ");
+		for (i = 1; i < size; i++) {
 			
-		//}
-		//for (j = 0; j < 6; j++) {
-			//printf(" before send  %d, ", arr[i][j]);
-		//}
-		//for (j = 0; j < 6; j++) {
-			//printf(" before send  %d, ", arr[1][j]);
-		//}
-		//for (i = 1; i < size; i++) {
-			//if (i < size) {
-				//MPI_Send(arr[0], 6, MPI_INT, 1, 1, MPI_COMM_WORLD);
-			//}
-		//}
-		MPI_Send(rnd_num, 512, MPI_INT, 1, 1, MPI_COMM_WORLD);
-		//MPI_Send(rnd_num, 512, MPI_INT, 2, 2, MPI_COMM_WORLD);
-	} else if(my_rank==1){
-		printf("loops %d, rank %d \n", num_loop, my_rank);
-	//} else {
-		//process other than root process
-		//for (i = 1; i < size; i++) {
-			//MPI_Recv(arr[1], 6, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-			//for (j = 0; j < 6; j++) {
-			//	printf("%d, ", arr[0][j]);
-			//}
-			MPI_Recv(rnd_num, 512, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-			
-			for (j = 0; j < num_loop; j++) {
-				printf("%d, ", rnd_num[j]);
-			}
-		//}
-			printf("\n ");
-	} 
-	/*else if (my_rank == 2) {
-		printf("loops %d, rank %d \n", num_loop, my_rank);
-		MPI_Recv(rnd_num, 512, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
-		for (j = num_loop-6; j < num_loop; j++) {
-			printf("%d, ", rnd_num[j]);
+				MPI_Send(rnd_num, 512, MPI_INT, i, i, MPI_COMM_WORLD);
+				MPI_Recv(new_mul_arr, 6,MPI_INT,i, i, MPI_COMM_WORLD,&status);
+				printf("recieved from process %d to 0: ", i);
+				for (j = 0; j < 6; j++) {
+					printf("%d, ", new_mul_arr[j]);
+				}
+				printf("\n \n");
 		}
-		printf("\n ");
-	}*/
+		
+		
+	} else {
+		
+		
+		for (i = 1; i < size; i++) {
+			if (i == my_rank) {
+				MPI_Recv(rnd_num, 512, MPI_INT, 0, i, MPI_COMM_WORLD, &status);
+				printf("recieved from process 0 to %d : ",i);
+				for (j = loop_begin; j < loop_end; j++) {
+					printf("%d, ", rnd_num[j]);
+					new_mul_arr[j - loop_begin] = rnd_num[j] * 3;
+					
+				}
+				
+				MPI_Send(new_mul_arr, 6, MPI_INT, 0, i, MPI_COMM_WORLD);
+				printf("\n ");
+			}
+		}
+	}
 	MPI_Finalize();
 }
